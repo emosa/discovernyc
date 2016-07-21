2ea92543b349af6dacd0cd2cfd51ad8e976124b2
emosa <iglitzi@fullsail.edu>
2016-07-20 22:14:55 -0400

    changeFONTS

--- js/global.js
+++ js/global.js
@@ -6,7 +6,7 @@ $( function() {
 	$('.countdown.styled').countdown({
 	  date: endDate,
 	  render: function(data) {
-		$(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+		$(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>Days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>Hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>Min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>Sec</span></div>");
 	  }
 	});
 	$('.countdown.callback').countdown({
